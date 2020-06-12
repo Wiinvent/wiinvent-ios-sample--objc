e0326133eb09022edf80bc5cd639fe1021ac2aec
Dang Nam <dainam8x@gmail.com>
2020-06-12 18:20:40 +0700

    add id

--- wiinvent-ios-sample-objective-c/ViewController.m
+++ wiinvent-ios-sample-objective-c/ViewController.m
@@ -11,9 +11,9 @@
 #import <AVFoundation/AVFoundation.h>
 #import <WISDK/WISDK-Swift.h>
 
-static NSInteger const SAMPLE_ACCOUNT_ID = 1;
+static NSInteger const SAMPLE_ACCOUNT_ID = 81;
 static NSString * const SAMPLE_CHANNEL_ID = @"34";
-static NSString * const SAMPLE_STREAM_ID = @"";
+static NSString * const SAMPLE_STREAM_ID = @"154";
 static NSString * const SAMPLE_TOKEN = @"1";
 static NSString * const SAMPLE_VOD_URL = @"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4";
 
@@ -90,7 +90,7 @@ static NSString * const SAMPLE_VOD_URL = @"http://commondatastorage.googleapis.c
         WIOverlayData *overlayData = [[WIOverlayData alloc]
                                       initWithChannelId: SAMPLE_CHANNEL_ID
                                       streamId: SAMPLE_STREAM_ID
-                                      thirdPartyToken: @"02dcd94bc30f0b49b174751837aa52184c9dd2af"
+                                      thirdPartyToken: @"025e2286d574f18e362bcb1f54dda8124eb6ceba"
                                       accountId: SAMPLE_ACCOUNT_ID
                                       mappingType: MappingTypeTHIRDPARTY
                                       platform: nil
@@ -101,7 +101,7 @@ static NSString * const SAMPLE_VOD_URL = @"http://commondatastorage.googleapis.c
         // Add overlays to player view.
         [WISDK addOverlaysToPlayerViewWithContainer:self.view frame: heightPlayer overlayData:overlayData];
         
-        double delayInSeconds = 5.0;
+        double delayInSeconds = 10.0;
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
              
